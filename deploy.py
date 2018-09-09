1a64559356f3527be94f26197412537480560c1c
Xavier Mayeur <xavier@mayeur.be>
2018-09-09 18:38:46 +0200

    Send CPU usage, MEM usage and temperature

---
+++ deploy.py
@@ -0,0 +1,39 @@
+import os.path
+from py_compile import compile
+from time import sleep
+
+from fabric.api import *
+
+# from fabric.contrib.project import rsync_project as rsync
+
+
+env.host_string = '192.168.0.4'
+env.user = 'root'
+env.use_ssh_config = True
+
+# Deploy files
+deploy_list = ['info.py', ]
+project = 'piinfo'
+config = project + '.conf'
+
+for f in deploy_list:
+    if f.find('.py') > 0:
+        compile(f)
+        put(local_path=f + 'c', remote_path='~/' + project + '/' + f + 'c', use_sudo=True, mode='0755')
+    else:
+        put(local_path=f, remote_path='~/' + project + '/' + f, use_sudo=True, mode='0755')
+
+# put (local_path='wifi_rebooter.sh', remote_path='/usr/local/bin/checkwifi.sh' , use_sudo=True, mode='0755')
+
+# Stop service, update it and re-start
+with settings(warn_only=True):
+    run('sudo service ' + project + ' stop')
+
+with cd('/etc/init.d'):
+    put(local_path=project + '.', remote_path=os.path.join('/etc/init.d/', project), use_sudo=True, mode='0755')
+    run('sudo update-rc.d ' + project + ' defaults')
+    run('sudo systemctl enable ' + project)
+
+run('sudo service ' + project + ' start')
+sleep(5)
+run('sudo service ' + project + ' status')
---
+++ info.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python
+from __future__ import division
+
+import socket
+from subprocess import PIPE, Popen
+from time import sleep
+
+import paho.mqtt.client as mqtt
+import psutil
+import requests
+
+# from sys import exit
+
+HOST = '192.168.0.4'
+vault_url = 'http://192.168.0.4:5000/api/ID'
+uid = 'iot'
+
+
+def get_vault(uid):
+    # url = config.get('vault', 'vault_url')
+    
+    r = requests.get(url=vault_url + '?uid=%s' % uid)
+    id = r.json()
+    r.close()
+    if id['status'] == 200:
+        _username = id['username']
+        _password = id['password']
+    else:
+        _username = ''
+        _password = ''
+    return _username, _password
+
+
+def get_cpu_temperature():
+    process = Popen(['vcgencmd', 'measure_temp'], stdout=PIPE)
+    output, _error = process.communicate()
+    return float(output[output.index('=') + 1:output.rindex("'")])
+
+
+def main():
+    uname, pwd = get_vault(uid)
+    hname = socket.gethostname()
+    client = mqtt.Client('info')
+    client.username_pw_set(username=uname, password=pwd)
+    try:
+        client.connect(HOST, port=1883)
+    except:
+        print('Cannot connect to mqtt broker')
+    
+    while True:
+        
+        cpu_temperature = get_cpu_temperature()
+        # print('cpu temperature: ' + str(cpu_temperature) + 'C')
+        try:
+            client.publish(hname + '/temperature', str(cpu_temperature), qos=0, retain=False)
+        except:
+            try:
+                client.connect(HOST, port=1883)
+                continue
+            except:
+                print('Cannot connect to mqtt broker')
+                sleep(15)
+                continue
+        
+        cpu_usage = psutil.cpu_percent()
+        # print('cpu usage: ' + str(cpu_usage) + '%')
+        client.publish(hname + '/CPU%', str(cpu_usage), qos=0, retain=False)
+        
+        ram = psutil.virtual_memory()
+        ram_total = ram.total / 2 ** 20  # MiB.
+        ram_used = ram.used / 2 ** 20
+        ram_free = ram.free / 2 ** 20
+        ram_percent_used = ram.percent
+        # print('free ram: ' + str(100-ram_percent_used) + '%')
+        client.publish(hname + '/freeRAM%', str(100 - ram_percent_used), qos=0, retain=False)
+        
+        disk = psutil.disk_usage('/')
+        disk_total = disk.total / 2 ** 30  # GiB.
+        disk_used = disk.used / 2 ** 30
+        disk_free = disk.free / 2 ** 30
+        disk_percent_used = disk.percent
+        #
+        # Print top five processes in terms of virtual memory usage.
+        #
+        # processes = sorted(
+        #     ((p.get_memory_info().vms, p) for p in psutil.process_iter()),
+        #    reverse=True
+        # )
+        # for virtual_memory, process in processes[:5]:
+        #    print virtual_memory // 2**20, process.pid, process.name
+        sleep(15)
+
+
+if __name__ == '__main__':
+    main()
